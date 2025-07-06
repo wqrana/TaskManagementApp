aeabcb18134d7eb062247be7eb5f0db31a7a4a35
Waqar <Waqar@DSL-WAQARQ>
2025-07-06 14:13:46 +0500

    move the hard-coded api url in enviroment.ts file

--- AngularApp/src/app/services/taskService.ts
+++ AngularApp/src/app/services/taskService.ts
@@ -2,30 +2,33 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/task';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  apiUrl="https://localhost:44368/api/Tasks"
-  constructor(private http: HttpClient) { }
+  private readonly apiEndpoint:string = `${environment.apiBaseUrl}/Tasks`;;
+  constructor(private http: HttpClient) { 
+       
+  }
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+    return this.http.get<Task[]>(this.apiEndpoint);
   }
   getTask(id:number): Observable<Task> {
-    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+    return this.http.get<Task>(`${this.apiEndpoint}/${id}`);
   }
   addTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task);
+    return this.http.post<Task>(this.apiEndpoint, task);
   }
 
   updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+    return this.http.put<Task>(`${this.apiEndpoint}/${task.id}`, task);
   }
 
   deleteTask(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiEndpoint}/${id}`);
   }
 
 }
